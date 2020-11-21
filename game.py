bc0894336516f766dc405b7bc8ff6c90fd98ca09
Jacob Eason <31490880+JEason13@users.noreply.github.com>
2020-11-21 15:32:13 -0500

    Add files via upload

---
+++ game.py
@@ -0,0 +1,125 @@
+#game.py
+import os
+from colorama import Fore, Back, Style
+from termcolor import colored, cprint
+import time
+
+enemy_ship = colored("O", 'red', attrs=['reverse', 'blink', 'bold'])
+player_ship = colored( "∆", 'cyan', attrs=['reverse', 'blink', 'bold'])
+#player_ship_shielded_full = "((∆))"
+#player_ship_shielded_half = "(∆)"
+bullet = colored( ".", 'green', attrs=['reverse', 'blink', 'bold'])
+
+def game():
+    name = input("ENTER YOUR NAME:\t")
+
+    user_score = 0
+
+    game_map = [ [0, 0, 0, 0, 0, 0, 0, 0 ],  
+    [ 0, 0, 0, 0, 0, 0, 0, 0 ],  
+    [ 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0 ], 
+    [ 0, 0, 0, 0, 0, 0, 0, 0 ], 
+    [ 0, 0, 0, 0, 0, 0, 0, 0 ], 
+    [ 0, 0, 0, 0, 0, 0, 0, 0 ],   
+    [ 0, 0, 0, 0, 0, 0, 0, 0 ] ]  
+
+    player_ship_pos = game_map[7][3] = player_ship
+    enemy_ship_pos0 = game_map[0][1] = enemy_ship
+    enemy_ship_pos1 = game_map[0][3] = enemy_ship
+    enemy_ship_pos2 = game_map[0][5] = enemy_ship
+    
+    bullet_pos = game_map[6][3] = bullet
+
+
+
+    for i in range(len(game_map)) :  
+        for j in range(len(game_map[i])) :  
+            print(Fore.BLACK, Back.BLACK, game_map[i][j], end=" ") 
+        print()  
+
+
+    shoot_yn = input("SHOOT? (Y/N):\t")
+    if(shoot_yn == 'n' or shoot_yn == 'N'):
+        pass
+    elif(shoot_yn == 'y' or shoot_yn == 'Y'):
+        game_map[6][3] = 0
+        bullet_pos = game_map[5][3] = bullet
+        time.sleep(0.2)
+        bullet_pos = game_map[4][3] = bullet
+        time.sleep(0.2)
+        bullet_pos = game_map[3][3] = bullet
+        time.sleep(0.2)        
+        bullet_pos = game_map[2][3] = bullet
+        time.sleep(0.2)        
+        bullet_pos = game_map[1][3] = bullet
+        time.sleep(0.2)        
+        bullet_pos = game_map[0][3] = bullet
+        if(bullet_pos == game_map[0][3]):
+            print(Fore.GREEN, Back.BLACK, "Yay! You scored a point! Good job!")
+            user_score = user_score + 1
+            lb_file = 'leaderboard.txt'
+            lb_data =("\t"+name+": "+ str(user_score))
+            lb = open(lb_file, 'a')
+            lb.write(lb_data)
+            lb.close
+
+
+    dir_input = input("MOVE (WASD):\t")
+    if(dir_input == 'a' or dir_input == 'A'):
+        os.system('clear')
+        game_map[7][3] = 0
+        player_ship_pos = game_map[7][2] = player_ship
+        for i in range(len(game_map)) :  
+            for j in range(len(game_map[i])) :  
+                print(Fore.BLACK, Back.BLACK, game_map[i][j], end=" ") 
+            print()
+        shoot_yn = input("SHOOT? (Y/N):\t")
+        if(shoot_yn == 'n' or shoot_yn == 'N'):
+            pass
+        elif(shoot_yn == 'y' or shoot_yn == 'Y'):
+            pass
+  
+
+    if(dir_input == 'd' or dir_input == 'D'):
+        os.system('clear')
+        game_map[7][3] = 0
+        player_ship_pos = game_map[7][4] = player_ship
+        for i in range(len(game_map)) :  
+            for j in range(len(game_map[i])) :  
+                print(Fore.BLACK, Back.BLACK, game_map[i][j], end=" ") 
+            print()  
+        shoot_yn = input("SHOOT? (Y/N):\t")
+        if(shoot_yn == 'n' or shoot_yn == 'N'):
+            pass
+        elif(shoot_yn == 'y' or shoot_yn == 'Y'):
+            pass
+
+    if(dir_input == 'w' or dir_input == 'W'):
+        os.system('clear')
+        game_map[7][3] = 0
+        player_ship_pos = game_map[6][3] = player_ship
+        for i in range(len(game_map)) :  
+            for j in range(len(game_map[i])) :  
+                print(Fore.BLACK, Back.BLACK, game_map[i][j], end=" ") 
+            print()  
+        shoot_yn = input("SHOOT? (Y/N):\t")
+        if(shoot_yn == 'n' or shoot_yn == 'N'):
+            pass
+        elif(shoot_yn == 'y' or shoot_yn == 'Y'):
+            pass
+
+    if(dir_input == 's' or dir_input == 'S'):
+        os.system('clear')
+        game_map[7][3] = 0
+        player_ship_pos = game_map[7][3] = player_ship
+        print(Fore.GREEN, Back.BLACK, "You can not move here!")
+    
+        shoot_yn = input("SHOOT? (Y/N):\t")
+        if(shoot_yn == 'n' or shoot_yn == 'N'):
+            pass
+        elif(shoot_yn == 'y' or shoot_yn == 'Y'):
+            pass
+
+
+
--- main.py
+++ main.py
@@ -5,8 +5,16 @@ import os
 import random 
 import time 
 from colorama import Fore, Back, Style
+
 from assets import ship_logo
 from assets import galaga_logo
+from assets import main_menu_text
+
+from game import game
+
+lb = open('leaderboard.txt', 'r')
+lb_contents = lb.read()
+
 
 os.system('clear') #clear the screen
 os.system("\x1b[40m") #set background color to black
@@ -15,17 +23,52 @@ def splash():
     print(Fore.RED + galaga_logo)
     time.sleep(1)
     os.system('clear')
-    print(Fore.BLUE + galaga_logo)
+    print(Fore.CYAN + galaga_logo)
     time.sleep(1)
     os.system('clear')
     print(Fore.RED + galaga_logo)
     time.sleep(1)
     os.system('clear')
-    print(Fore.BLUE + galaga_logo)
+    print(Fore.CYAN + galaga_logo)
     time.sleep(1)
     os.system('clear')
 
     print(Fore.RED + ship_logo)
 
+def how_to_play():
+    print(Fore.GREEN + "\n\n\tThis game is a clone of the 1981 game Galaga. \n\tIn this game you will have to pilot a spaceship and attempt to avoid dangers such as enemy alien ships and their gunfire. \n\tUse your gun to shoot the enemy ships and earn points. But be caeful, you only have three lives!\n\n\n\n")
+
+def quit_game():
+    print(Fore.GREEN + "\n\n\n\tThanks for playing! See you again soon!\n\n")
+
+def main_menu(): 
+    print(Fore.RED + main_menu_text)
+    time.sleep(1)
+    print(Fore.YELLOW + "\t• START GAME  (1)\n")
+    print(Fore.YELLOW + "\t• LEADERBOARD (2)\n")
+    print(Fore.YELLOW + "\t• HOW TO PLAY (3)\n")
+    print(Fore.YELLOW + "\t• QUIT        (4)\n")
+    main_menu_input = str(input("\tENTER THE NUMBER OF YOUR CHOICE:\t"))
+    if(main_menu_input == '4'):
+        quit_game()
+    elif(main_menu_input == '3'):
+        how_to_play()
+        time.sleep(15)
+        os.system('clear')
+        main_menu()
+    elif(main_menu_input == '2'):
+        print(Fore.GREEN + "\n\t" + lb_contents)
+        lb.close
+        time.sleep(15)
+        os.system('clear')
+        main_menu()
+    elif(main_menu_input == '1'):
+        os.system('clear')
+        print(Fore.GREEN + "")
+        game()
+        print(Fore.GREEN + "")
+
+
 
 splash()
+main_menu()
