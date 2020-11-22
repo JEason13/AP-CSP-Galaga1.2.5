639e21e63c0f053c88686fbfe32d3e20bcb77969
Jacob Eason <31490880+JEason13@users.noreply.github.com>
2020-11-22 15:04:29 -0500

    Add files via upload

--- game.py
+++ game.py
@@ -8,7 +8,7 @@ enemy_ship = colored("O", 'red', attrs=['reverse', 'blink', 'bold'])
 player_ship = colored( "∆", 'cyan', attrs=['reverse', 'blink', 'bold'])
 #player_ship_shielded_full = "((∆))"
 #player_ship_shielded_half = "(∆)"
-bullet = colored( ".", 'green', attrs=['reverse', 'blink', 'bold'])
+bullet = colored( ".", 'green', attrs=['bold'])
 
 def game():
     name = input("ENTER YOUR NAME:\t")
@@ -38,7 +38,7 @@ def game():
             print(Fore.BLACK, Back.BLACK, game_map[i][j], end=" ") 
         print()  
 
-
+    print(Fore.GREEN, Back.BLACK, "Welcome to galaga! Use Y/N to shoot and WASD to move when prompted!")
     shoot_yn = input("SHOOT? (Y/N):\t")
     if(shoot_yn == 'n' or shoot_yn == 'N'):
         pass
@@ -59,7 +59,7 @@ def game():
             print(Fore.GREEN, Back.BLACK, "Yay! You scored a point! Good job!")
             user_score = user_score + 1
             lb_file = 'leaderboard.txt'
-            lb_data =("\t"+name+": "+ str(user_score))
+            lb_data =("\t"+name+": "+ str(user_score)+"\n")
             lb = open(lb_file, 'a')
             lb.write(lb_data)
             lb.close
@@ -74,6 +74,7 @@ def game():
             for j in range(len(game_map[i])) :  
                 print(Fore.BLACK, Back.BLACK, game_map[i][j], end=" ") 
             print()
+        print(Fore.GREEN, Back.BLACK, "Good job! now you know how to play!")
         shoot_yn = input("SHOOT? (Y/N):\t")
         if(shoot_yn == 'n' or shoot_yn == 'N'):
             pass
@@ -81,7 +82,7 @@ def game():
             pass
   
 
-    if(dir_input == 'd' or dir_input == 'D'):
+    elif(dir_input == 'd' or dir_input == 'D'):
         os.system('clear')
         game_map[7][3] = 0
         player_ship_pos = game_map[7][4] = player_ship
@@ -89,13 +90,14 @@ def game():
             for j in range(len(game_map[i])) :  
                 print(Fore.BLACK, Back.BLACK, game_map[i][j], end=" ") 
             print()  
+        print(Fore.GREEN, Back.BLACK, "Good job! now you know how to play!")
         shoot_yn = input("SHOOT? (Y/N):\t")
         if(shoot_yn == 'n' or shoot_yn == 'N'):
             pass
         elif(shoot_yn == 'y' or shoot_yn == 'Y'):
             pass
 
-    if(dir_input == 'w' or dir_input == 'W'):
+    elif(dir_input == 'w' or dir_input == 'W'):
         os.system('clear')
         game_map[7][3] = 0
         player_ship_pos = game_map[6][3] = player_ship
@@ -103,18 +105,18 @@ def game():
             for j in range(len(game_map[i])) :  
                 print(Fore.BLACK, Back.BLACK, game_map[i][j], end=" ") 
             print()  
+        print(Fore.GREEN, Back.BLACK, "Good job! now you know how to play!")
         shoot_yn = input("SHOOT? (Y/N):\t")
         if(shoot_yn == 'n' or shoot_yn == 'N'):
             pass
         elif(shoot_yn == 'y' or shoot_yn == 'Y'):
             pass
 
-    if(dir_input == 's' or dir_input == 'S'):
+    elif(dir_input == 's' or dir_input == 'S'):
         os.system('clear')
         game_map[7][3] = 0
         player_ship_pos = game_map[7][3] = player_ship
         print(Fore.GREEN, Back.BLACK, "You can not move here!")
-    
         shoot_yn = input("SHOOT? (Y/N):\t")
         if(shoot_yn == 'n' or shoot_yn == 'N'):
             pass
